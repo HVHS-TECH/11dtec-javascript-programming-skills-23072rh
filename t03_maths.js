ef88756f2a1e8bb3ca0f97a4a3d55c1745041b91
23072rh <23072rh@hvhs.school.nz>
2025-05-01 00:46:24 +0000

    May 1, 2025, 12:46 AM

--- t03_maths.js
+++ t03_maths.js
@@ -13,12 +13,20 @@ console.log (Year);
 var Age = 15;
 console.log (Age);
 
+var Num1 = 10;
+
+var Num2 = 2;
+
+var Answer;
+
 var Pocketmoney = 3;
 console.log (Pocketmoney);
 
 console.log("Hi Reuben");
 console.log("As of", Year, "You are", Age);
+console.log("In 10 years you will be", Age + Num1 = Answer, "years old")
 console.log("You have", Pocketmoney, "dollars");
+
 /***********************************************************************
  Main code
 ***********************************************************************/
