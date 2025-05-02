9de50116efc5a360f43071039d1e447c9c95001d
23072rh <23072rh@hvhs.school.nz>
2025-05-02 02:23:24 +0000

    May 2, 2025, 2:23 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,41 @@
+/***********************************************************************
+ Name of task: Intro to Java Script and setting up the Java Script file
+ Name: Reuben Holdsworth
+ Date written: 29/4/2025
+ Purpose: Practicing with Java Script
+ **********************************************************************/
+ console.log("Running t01_introduction.js");
+ console.log("Adarsh is the goat");
+//Variables
+var Year = 2025;
+console.log (Year);
+
+var Born = 2009;
+
+var Age = 15;
+console.log (Age);
+
+var Num1 = 10;
+
+var Num2 = 2;
+
+var answer;
+
+var Pocketmoney = 5;
+console.log (Pocketmoney);
+
+console.log("Hi Reuben");
+console.log("As of", Year, "You are", Age);
+console.log("You were born in", Born);
+answer = Age + Num1;
+console.log("In 10 years you will be", answer, "years old")
+console.log("You have", Pocketmoney, "dollars");
+answer = Pocketmoney / Num2;
+console.log("Half of you money is", answer);
+/***********************************************************************
+ Main code
+***********************************************************************/
+
+/***********************************************************************
+ Functions
+***********************************************************************/
