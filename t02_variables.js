d2bc64602e6cb3942be2d2debb7fb7b7ddb415b4
23072rh <23072rh@hvhs.school.nz>
2025-05-02 02:24:53 +0000

    May 2, 2025, 2:24 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,7 +1,7 @@
 /***********************************************************************
- Name of task: Intro to Java Script and setting up the Java Script file
+ Name of task: Java script variables
  Name: Reuben Holdsworth
- Date written: 29/4/2025
+ Date written: 30/4/2025
  Purpose: Practicing with Java Script
  **********************************************************************/
  console.log("Running t01_introduction.js");
