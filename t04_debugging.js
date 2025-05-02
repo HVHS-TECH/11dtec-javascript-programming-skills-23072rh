2e173eb774319dfadd7ebbabb21bb62f1f62716d
23072rh <23072rh@hvhs.school.nz>
2025-05-02 02:39:18 +0000

    May 2, 2025, 2:39 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -12,7 +12,7 @@ var width = 10;
 
 // Display the results
 console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
-console.log("The same piece of paper has a perimeter of", area)
+console.log("The same piece of paper has a perimeter of "+perimeter)
 // Check, does this result look sensible
 if (area > perimeter) {
     console.log("Yay, your area is larger, this is right for this example")
