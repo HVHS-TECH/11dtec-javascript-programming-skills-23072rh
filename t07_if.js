8ce0186129221b793ad35dc92ebefe0b29c90cc0
23072rh <23072rh@hvhs.school.nz>
2025-05-08 00:54:47 +0000

    May 8, 2025, 12:54 AM

---
+++ t07_if.js
@@ -0,0 +1,28 @@
+/***********************************************************************
+ Name of task: Learning how to use arrays to store lots of values with Java Jcript
+ Name: Reuben Holdsworth
+ Date written: 8/5/2025
+ Purpose: Practicing with Java Script
+ **********************************************************************/
+ console.log("Running t01_alerts_prompts.js");
+/******** 
+ Varibles
+********/
+
+//Arrays
+let Chocoarray = ["Chocolate is yuck.", "Chocolate is meh.", "Chocolate is pretty good.", "Chocolate is the best thing ever!!!"];
+let choice = prompt("Pick a number from 0 to 3");
+/***********************************************************************
+ Main code
+***********************************************************************/
+//Variable logs
+console.log (Year);
+console.log (Age);
+console.log (Pocketmoney);
+//Code
+
+//Alerts & Prompts
+alert("Your chosen number was " +choice+ ". \nYou think " +Chocoarray[choice]);
+/***********************************************************************
+ Functions
+***********************************************************************/
