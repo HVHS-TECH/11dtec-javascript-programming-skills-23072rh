7e2af37c22b196a6caf12e7135ed98f70db39d16
23072rh <23072rh@hvhs.school.nz>
2025-05-09 02:58:49 +0000

    May 9, 2025, 2:58 AM

--- t07_if.js
+++ t07_if.js
@@ -8,24 +8,43 @@
 /******** 
  Varibles
 ********/
-
-
+//Year 
+var Year = 2025;
+var Born = 2009;
+//Numbers
+var Age = 15;
+var Num1 = 10;
+var Num2 = 2;
+var answer;
+var Pocketmoney = 5;
 /***********************************************************************
  Main code
 ***********************************************************************/
 //Variable logs
-
+console.log (Year);
+console.log (Age);
+console.log (Pocketmoney);
 //Code
-
+console.log("Hi Reuben");
+console.log("As of", Year, "You are", Age);
+console.log("You were born in", Born);
+answer = Age + Num1;
+console.log("In 10 years you will be", answer, "years old")
+console.log("You have", Pocketmoney, "dollars");
+answer = Pocketmoney / Num2;
+console.log("Half of you money is", answer);
 //Alerts & Prompts
-
-
-//Loop 0-5
-console.log("Starting the FOR loop");
-for (count = 0; count < 10; count ++) {
-console.log("Current Count: " + count);    
+prompt("Hi ", "Bob");
+alert("I predict you were born in " +Born);
+alert("Your pocket money right now is " +Pocketmoney+ " dollars");
+if(Pocketmoney >= 4) {
+alert("You CAN afford a chocolate bar")
+}else{
+alert("Sorry you CANT afford a chocolate bar")
 }
-console.log("Ending the FOR loop");
+
+
+
 /***********************************************************************
  Functions
 ***********************************************************************/
