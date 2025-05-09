f706f84c1c2af0954f35852080ae18682616fecd
23072rh <23072rh@hvhs.school.nz>
2025-05-09 02:59:48 +0000

    May 9, 2025, 2:59 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -16,7 +16,7 @@
 
 
 for (count = 0; count <= 5; count ++) {
-    var = prompt("give me a number");
+    var xxx = prompt("give me a number");
 }
 
 /***********************************************************************
