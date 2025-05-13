114fdafc8a66583d2e2837c741dbb767eab48aa6
23072rh <23072rh@hvhs.school.nz>
2025-05-13 23:35:02 +0000

    May 13, 2025, 11:35 PM

--- t09_functions.js
+++ t09_functions.js
@@ -13,12 +13,13 @@
 /***********************************************************************
  Main code
 ***********************************************************************/
+var num;
 var answer;
 for (count = 0; count <= 4; count ++) {
-    number = prompt("Give me a number between 0 - 10");
-    number = Number(num)
+    num = prompt("Give me a number");
+    num = Number(num)
 }
-answer = number+number+number+number+number
+answer = num+num+num+num+num
 console.log("The sum to your chosen numbers is " +answer);
 /***********************************************************************
  Functions
