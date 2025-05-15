45b857581771f90a545b9886fc6b0c380d6b6d58
23072rh <23072rh@hvhs.school.nz>
2025-05-15 01:03:05 +0000

    May 15, 2025, 1:03 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -12,7 +12,32 @@ console.log("Running t10_activate_via_button.js");
 /***********************************************************************
  Main code
 ***********************************************************************/
+let userName;
+let userAge;
+let userMoney;
 
 /***********************************************************************
  Functions
 ***********************************************************************/
+function start () {
+    userName = askUserName();
+    userAge = askUserAge();
+    userMoney = askMoney();
+}
+function askUserName() {
+    askUserName = prompt("Whats your name?");
+    alert("Hello " + askUserName + "!");
+}
+function askUserAge() {
+    askUserAge = prompt("How old are you " + askUserName + "?");
+    alert("You are " + askUserAge + " years old in " + year + ".");
+}
+function askUserMoney() {
+    var pocketMoney = askUserMoney = prompt("How much pocket money do you have " + askUserName + "?");
+    alert("So " + askUserName + " is " + askUserAge + " years old, and has " + pocketMoney + " dollars of pocket money as of " + year + ".");
+    if (pocketMoney >= 4) {
+        alert("You CAN afford a chocolate bar " + askUserName + "!");
+    } else {
+        alert("Sorry you CANT afford a chocolate bar " + askUserName + ".");
+    }
+}
