2d0b49bc1baf31587c7c6fd5badc3627e22b7070
23072rh <23072rh@hvhs.school.nz>
2025-06-05 00:38:16 +0000

    Jun 5, 2025, 12:38 AM

--- t13_validation.js
+++ t13_validation.js
@@ -50,5 +50,5 @@ function start() {
   alert("In the store today we have 5 chocolate bars ranging from 1 - 5 dollars:\nMars bar-$1\nMoro bar-$2\nKing Size Crunchy-$3\nDairy Milk Block-$4\nLindt Block-$5");
   //Asking the user how much pocket money they have
   pocketMoney = prompt("How much pocket money do you have " + userName + "?");
-  alert("So " + userName + " has" + pocketMoney + " dollars.");
+  alert("So " + userName + " has " + pocketMoney + " dollars.");
 }
